4b98fb9b64c6c0652c57f0f47eb5346b933ab994
MilosJovic23 <milos.mj992@gmail.com>
2025-01-31 11:12:37 +0100

    * cleared unused line of code

--- script.js
+++ script.js
@@ -2,22 +2,13 @@
 var proizvodi = ["audi", "mercedes", "BMW"];
 console.log(proizvodi[0], proizvodi[1], proizvodi[2]);
 
-// for petlje , while petlje , foreach
 
 for (var jedanProizvod in proizvodi) {
 	console.log(jedanProizvod, proizvodi[jedanProizvod]);
 }
 
-// testiranje
-
 var brojJedan = 1;
 
-//  var proizvod = 0;
-//  proizvodi[proizvod];
-//  var proizvod = 1;
-//  proizvodi[proizvod];
-//  var proizvod = 2;
-//  proizvodi[proizvod];
 
 var komponente = ["Maticna ploca", "Ram memorija", "Graficka kartica"];
 
@@ -26,7 +17,6 @@ for (var proizvod in komponente) {
 	document.getElementById("vezba").innerHTML += komponente[proizvod];
 }
 
-// ispisivanje podataka u html-u
 
 document.getElementById("listaKomponenata").innerHTML = "pozdrav";
 document.getElementById("listaKomponenata").innerHTML += " Milos";
